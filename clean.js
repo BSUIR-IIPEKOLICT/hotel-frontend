18cc0ae3c98d155be5b7c973f41ca4f236a18b5f
iipekolict <iipekolict@gmail.com>
2021-12-30 01:34:20 +0300

    feat: succefully migrate

--- clean.js
+++
@@ -1,9 +0,0 @@
-const { resolve } = require('path')
-const { readdir, rm } = require('fs/promises')
-;(async () => {
-  const files = await readdir(resolve())
-
-  if (files.includes('dist')) {
-    await rm(resolve('dist'), { recursive: true })
-  }
-})()
--- shared/constants.ts
+++ shared/constants.ts
@@ -1,5 +1,5 @@
 export const LOCAL_PORT = 5000
-export const LOCAL_MONGODB_URL = 'mongodb://localhost:27017/hotel'
+export const LOCAL_MONGODB_URL = 'mongodb://127.0.0.1:27017/hotel'
 export const LOCAL_JWT_SECRET = 'lol'
 
 export const SUCCESS_CONNECT_MONGO = 'Success connect to mongo'
